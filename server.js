2373d544c24fdddbc7c00f0da0afb0d954db8357
jlhiskey <jasonhi@crazyredhead.com>
2018-08-31 19:35:23 -0700

    hello world

---
+++ server.js
@@ -0,0 +1,30 @@
+'use strict';
+
+require('dotenv').config();
+const pg = require('pg');
+const express = require('express');
+const twilio = require('twilio');
+const PORT = process.env.PORT || 3000;
+const app = express();
+const conString = process.env.DATABASE_URL;
+
+app.use(express.urlencoded({extended:true}));
+
+const client = new pg.Client(conString);
+
+client.connect();
+
+app.set('view engine', 'ejs');
+
+//Routes
+
+app.get('/', (req, res) => {
+  homePage(req, res);
+});
+
+
+//Route Functions
+
+function homePage(req, res) {
+  res.render('master', {'thisPage':'partials/home.ejs', 'thisPageTitle':'Home'})
+}
