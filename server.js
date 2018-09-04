7a589c09a2019f4ba4ee858cec906bfae74781aa
jlhiskey <jasonhi@crazyredhead.com>
2018-09-04 11:25:47 -0700

    tuesday master

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 const conString = process.env.DATABASE_URL;
 
 app.use(express.urlencoded({extended:true}));
+app.listen(PORT, () => console.log('Server is up on ', PORT));
 
 const client = new pg.Client(conString);
 
