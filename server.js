0b505ae4c980b2a704938a1d618ce10845f053fe
trevorstam <trevorstam@gmail.com>
2018-09-04 20:46:24 -0700

    Just some test code. DO NOT MERGE INTO MASTER

--- server.js
+++ server.js
@@ -4,15 +4,22 @@ require('dotenv').config();
 const pg = require('pg');
 const express = require('express');
 const twilio = require('twilio');
+const googleMaps = require('@google/maps');
 const PORT = process.env.PORT || 3000;
 const app = express();
 const conString = process.env.DATABASE_URL;
 
 app.use(express.urlencoded({extended:true}));
-
+// Client for postgres
 const client = new pg.Client(conString);
 
 client.connect();
+
+// Client for google maps
+const gmClient = googleMaps.createClient({
+  key: process.env.GOOGLEMAPS_APIKEY,
+  Promise: Promise
+});
 
 app.set('view engine', 'ejs');
 
@@ -26,5 +33,20 @@ app.get('/', (req, res) => {
 //Route Functions
 
 function homePage(req, res) {
-  res.render('master', {'thisPage':'partials/home.ejs', 'thisPageTitle':'Home'})
+  res.render('master', {'thisPage':'partials/home.ejs', 'thisPageTitle':'Home'});
 }
+
+// Get Request to Google Geocoding API
+gmClient.geocode({
+  address: '1600 Amphitheatre Parkway, Mountain View, CA'
+  //parse JSON
+  //It should take in the street address, the city and State of database
+  //Add lat and long to the database table
+})
+  .asPromise()
+  .then((response) => {
+    console.log(response.json.results);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
